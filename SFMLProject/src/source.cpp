1512e3cda33db52b693e58cca3809ca34149aac2
Ricardo Alcantara <alcantarafox@yahoo.com.br>
2019-09-07 23:11:54 -0300

    Premake is so easy =D

--- SFMLProject/src/source.cpp
+++ SFMLProject/src/source.cpp
@@ -1,11 +1,19 @@
 #include <SFML/Graphics.hpp>
+#include <spdlog/spdlog.h>
 
 int main()
 {
+    #ifdef DEBUG
+    spdlog::set_level(spdlog::level::debug);
+    #endif
+    spdlog::info("Welcome to spdlog!");
+    spdlog::info("SFML works!");
+
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
+    spdlog::debug("Start main loop!");
     while (window.isOpen())
     {
         sf::Event event;
